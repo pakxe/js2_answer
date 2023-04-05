938a2421698aba568d8d0dab1dcb6eb6a0617b90
Pakxe <64801796+pakxe@users.noreply.github.com>
2023-04-05 22:25:46 +0900

    feat(problem1): 1번 문제 완료

---
+++ problem1.js
@@ -0,0 +1,11 @@
+// 1번 문제.
+
+let Name = "나현";
+
+alert(`hello ${1}`); // hello 1
+
+alert(`hello ${"Name"}`); // hello Name
+
+alert("hello ${Name}"); // hello ${Name}
+
+alert(`hello ${Name}`); // hello 나현
